5bdbf8832ec53c769fa08eab336803bb918bd103
sofiadinizms <sofiadinizms@MacBookProSofia.local>
2023-05-08 09:36:30 -0300

    first commit

---
+++ Barbie-world/Barbie_worldApp.swift
@@ -0,0 +1,17 @@
+//
+//  Barbie_worldApp.swift
+//  Barbie-world
+//
+//  Created by sofiadinizms on 08/05/23.
+//
+
+import SwiftUI
+
+@main
+struct Barbie_worldApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Barbie-world/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  Barbie-world
+//
+//  Created by sofiadinizms on 08/05/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
