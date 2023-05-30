3d1e06f2076ddcb884b37087558ad7bdf4770821
SofiRibeiro <sofiasantana168@gmail.com>
2023-05-30 10:39:51 -0300

    feat(feature/notifications): created notifications

--- Barbie-world/ContentView.swift
+++ Barbie-world/ContentView.swift
@@ -6,18 +6,78 @@
 //
 
 import SwiftUI
+import UserNotifications
+import UIKit
+
+class NotificationManager {
+    static let instance = NotificationManager()
+
+    func requestAuthorization() {
+        let options: UNAuthorizationOptions = [.alert, .sound, .badge]
+        UNUserNotificationCenter.current().requestAuthorization(options: options) { success, error in
+            if let error = error {
+                print("Error: \(error)")
+            } else {
+                print("Success")
+            }
+        }
+    }
+
+    func scheduleNotification(title: String, subtitle: String, hour: Int, minute: Int) {
+        let content = UNMutableNotificationContent()
+        content.title = title
+        content.subtitle = subtitle
+        content.sound = .default
+        content.badge = 1
+
+        var dateComponents = DateComponents()
+        dateComponents.hour = hour
+        dateComponents.minute = minute
+
+        let trigger = UNCalendarNotificationTrigger(dateMatching: dateComponents, repeats: true)
+
+        let request = UNNotificationRequest(
+            identifier: UUID().uuidString,
+            content: content,
+            trigger: trigger)
+        UNUserNotificationCenter.current().add(request)
+    }
+}
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        VStack{
+            Spacer()
         }
-        .padding()
+        .onAppear() {
+            NotificationManager.instance.requestAuthorization()
+            
+            NotificationManager.instance.scheduleNotification(
+                title: "Notificação 1",
+                subtitle: "Esta é a primeira notificação",
+                hour: 10,
+                minute: 20
+            )
+            
+            NotificationManager.instance.scheduleNotification(
+                title: "Notificação 2",
+                subtitle: "Esta é a segunda notificação",
+                hour: 10,
+                minute: 21
+            )
+            
+            NotificationManager.instance.scheduleNotification(
+                title: "Notificação 3",
+                subtitle: "Esta é a terceira notificação",
+                hour: 10,
+                minute: 22
+            )
+            
+            UIApplication.shared.applicationIconBadgeNumber = 0
+        }
     }
 }
+
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
