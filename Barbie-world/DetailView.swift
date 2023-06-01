0ea6edabf17843005247a151b37f3e466b7271ca
natalialbuquerque <natalialbuquerquee@gmail.com>
2023-06-01 11:38:16 -0300

    feat(feature/nome-da-oa): cep + r added

--- Barbie-world/DetailView.swift
+++ Barbie-world/DetailView.swift
@@ -10,15 +10,35 @@ import SwiftUI
 struct DetailView: View {
     var body: some View {
         ScrollView(.vertical, showsIndicators: false){
-                
+            
             VStack(alignment: .center){
-                    OAname()
-                    SubOA()
-                    Success()
+                OAname()
+                SubOA()
+                Success()
                 Progress(isCompleted: true)
+                CardsView()
+                Spacer()
+                HStack{
                     Spacer()
-//
+                    NavigationLink(destination: FilledHomeView(), label: {
+                        Image("Cancelar")
+                            .resizable()
+                            .scaledToFit()
+                    }).buttonStyle(PlainButtonStyle())
+                        .frame(width: 170, height: 80)
+                        .padding(.trailing, 20)
+                    
+                    NavigationLink(destination: FilledHomeView(), label: {
+                        Image("Criar")
+                            .resizable()
+                            .scaledToFit()
+                    }).buttonStyle(PlainButtonStyle())
+                        .frame(width: 170, height: 80)
+
                 }
+                .frame(width: 952, height: 202)
+                
+            }
         }
     }
 }
