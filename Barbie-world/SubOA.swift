581f9988492d906111673908f85ed1d8cd253890
sofiadinizms <sofiadinizms@MacBookProSofia.local>
2023-06-01 12:30:49 -0300

    adjusted suboas

--- Barbie-world/SubOA.swift
+++ Barbie-world/SubOA.swift
@@ -7,60 +7,72 @@
 
 import SwiftUI
 
+
 struct SubOA: View {
     
     struct SubOA {
-      var descript: String
-      var done: Bool
+        var descript: String
+        var done: Bool
     }
-
+    
     @State var items: [SubOA] = [
-      SubOA(descript: "ddd0", done: true),
-      SubOA(descript: "ddd1", done: true),
-      SubOA(descript: "ddd2", done: true),
-      SubOA(descript: "ddd3", done: true),
-      SubOA(descript: "ddd4", done: false),
-      SubOA(descript: "ddd4", done: false),
-      SubOA(descript: "ddd4", done: false),
-      SubOA(descript: "ddd4", done: false),
-      SubOA(descript: "ddd4", done: false),
-      SubOA(descript: "ddd4", done: false),
-      SubOA(descript: "ddd4", done: false),
-      SubOA(descript: "ddd4", done: false),
-      
+        SubOA(descript: "ddd0", done: true),
+        SubOA(descript: "ddd1", done: true),
+        SubOA(descript: "ddd2", done: true),
+        SubOA(descript: "ddd0", done: true),
+        SubOA(descript: "ddd1", done: true),
+        SubOA(descript: "ddd2", done: true),
+        SubOA(descript: "kjvnkdjnvkdjfnkjdf", done: true),
+        SubOA(descript: "ddd1", done: true),
+        SubOA(descript: "ddd2", done: true),
+        SubOA(descript: "ddd0", done: true),
+        SubOA(descript: "ddd1", done: true),
+        SubOA(descript: "ddd2", done: true)
+        
     ]
+
+   let columns = [
+    GridItem(.adaptive(minimum: 250))
+   ]
+    
+    
     var body: some View {
-        HStack {
-          ForEach(0...(items.count - 1), id: \.self) { i in
-            HStack{
-              Image(items[i].done ? "checkedsquare" : "square")
-                .onTapGesture {
-                  print("tocou")
-                  items[i].done = changeValue(val: items[i].done)
-                  print(items[i].done)
-                }
-              TextField("nome da oa", text: $items[i].descript)
-                .textFieldStyle(.plain)
-                .foregroundColor(.black)
+                    LazyVGrid(columns: columns, spacing: 20) {
+                        ForEach(0...(items.count - 1), id: \.self) { i in
+                            
+                            HStack {
+
+                                Image(items[i].done ? "checkedsquare" : "square")
+                                    .onTapGesture {
+                                        items[i].done = changeValue(val: items[i].done)
+
+                                    }
+                                TextField("nome da oa", text: $items[i].descript)
+                                    .fixedSize(horizontal: false, vertical: true)
+//                                    .frame(minWidth: 0, maxWidth: .infinity)
+                                    .textFieldStyle(.plain)
+                                    .foregroundColor(.black)
 
 
-            }
-            .padding()
-            .background(Color.white)
-            .cornerRadius(10)
-            .border(.gray)
-
-          }
-        }
-        .frame(maxWidth: 952, maxHeight: 202)
-//        .padding()
+                            } .padding()
+//                                .frame(width: 150)
+                                .background(Color.white)
+                                .border(.black.opacity(0.1))
+                                .cornerRadius(15)
+                                .shadow(color: Color.black.opacity(0.1), radius: 5)
+                            
+                    
+                        }
+                    }
+                    .frame(maxWidth: 952)
+        
     }
 }
 
 func changeValue(val: Bool) -> Bool {
     let newVal = !val
-
-  return newVal
+    
+    return newVal
 }
 
 struct SubOA_Previews: PreviewProvider {
