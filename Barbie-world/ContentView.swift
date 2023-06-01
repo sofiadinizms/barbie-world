8e49b17b4ba2720c1a28101dae60c4215a148c9c
SofiRibeiro <sofiasantana168@gmail.com>
2023-06-01 11:53:41 -0300

    improve the notifications

--- Barbie-world/ContentView.swift
+++ Barbie-world/ContentView.swift
@@ -45,32 +45,57 @@ class NotificationManager {
 }
 
 struct ContentView: View {
+    
+    @State var hasOA: Bool = true
+    
     var body: some View {
-        VStack{
-            Spacer()
+        ZStack{
+            
+            if hasOA{
+                
+                Color(.windowBackgroundColor)
+                
+            } else {
+                
+                Color(red:255.0, green: 255.0, blue: 255.0)
+            }
+            
+            ScrollView(.vertical, showsIndicators: false){
+                
+                if hasOA{
+                    
+                    FilledHomeView()
+                    
+                } else {
+                    
+                   EmptyHomeView()
+                    
+                }
+            }
+            
         }
         .onAppear() {
             NotificationManager.instance.requestAuthorization()
             
             NotificationManager.instance.scheduleNotification(
-                title: "Notificação 1",
-                subtitle: "Esta é a primeira notificação",
-                hour: 10,
-                minute: 20
+                title: "Um novo dia pra continuar trilhando!",
+                subtitle: "Aproveite o começo do dia para continuar os estudos e progredir na sua jornada de aprendizado.",
+                hour: 8,
+                minute: 30
             )
             
             NotificationManager.instance.scheduleNotification(
-                title: "Notificação 2",
-                subtitle: "Esta é a segunda notificação",
-                hour: 10,
-                minute: 21
+                title: "Como está esse começo de tarde?",
+                subtitle: "Aproveite o tempinho pós almoço para refletir sobre seus avanços até aqui. Não desista, continue trilhando!",
+                hour: 13,
+                minute: 30
             )
             
             NotificationManager.instance.scheduleNotification(
-                title: "Notificação 3",
-                subtitle: "Esta é a terceira notificação",
-                hour: 10,
-                minute: 22
+                title: "Que o dia tenha sido ótimo!",
+                subtitle: "Parabéns por mais um dia trilhando a sua jornada de aprendizado. Lembre de registrar tudo que você fez hoje pra alcançar seu OA.",
+                hour: 17,
+                minute: 30
             )
             
             UIApplication.shared.applicationIconBadgeNumber = 0
