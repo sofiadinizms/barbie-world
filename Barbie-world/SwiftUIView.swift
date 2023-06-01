10683054ac50ea54c6cbccfc8168d36c9371402c
Jpsmor <jpedrosmoreira1409@gmail.com>
2023-06-01 09:33:41 -0300

    feat(feature/coredata): created coredata base and functions

---
+++ Barbie-world/SwiftUIView.swift
@@ -0,0 +1,20 @@
+//
+//  SwiftUIView.swift
+//  Barbie-world
+//
+//  Created by Jpsmor on 01/06/23.
+//
+
+import SwiftUI
+
+struct SwiftUIView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct SwiftUIView_Previews: PreviewProvider {
+    static var previews: some View {
+        SwiftUIView()
+    }
+}
